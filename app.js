96514f1a8102c29cdb3f9e961908c0ace2704a54
Hank Adler <henryaguila1988@gmail.com>
2022-05-31 18:21:40 -0400

    disable helmet csp

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ if (nodeEnv === "dev") app.use(morgan("dev"));
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 /* routes */
 app.use(express.static("../client/dist"));
