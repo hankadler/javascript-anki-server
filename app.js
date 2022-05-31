95657f8c822123deebd23ff6aa019c4552ce633a
Hank Adler <henryaguila1988@gmail.com>
2022-05-31 15:42:05 -0400

    add helmet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import cors from "cors";
+import helmet from "helmet";
 import express from "express";
 import morgan from "morgan";
 import restrictAccess from "./wares/restrictAccess";
@@ -15,6 +16,7 @@ if (nodeEnv === "dev") app.use(morgan("dev"));
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(helmet());
 
 /* routes */
 app.use(express.static("../client/dist"));
