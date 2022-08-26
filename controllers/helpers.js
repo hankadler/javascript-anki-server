4bc91746a52e018e74f71d51d4a36a553e3391ff
Hank Adler <henryaguila1988@gmail.com>
2022-08-26 13:39:42 -0400

    update sendEmail to outlook defaults

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -136,29 +136,22 @@ const selectFields = async (arr, queryOpts) => {
 };
 
 const sendEmail = async (to, subject, message, isPlain = false) => {
-  let auth = {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  };
-  if (process.env.NODE_ENV === "prod") {
-    auth = {
-      type: "OAuth2",
-      user: process.env.EMAIL_USER,
-      clientId: process.env.EMAIL_CLIENT_ID,
-      clientSecret: process.env.EMAIL_CLIENT_SECRET,
-      refreshToken: process.env.EMAIL_REFRESH_TOKEN
-    };
-  }
   const transporter = createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT, 10),
-    secure: true,
-    auth
+    secure: false,
+    tls: {
+      ciphers: "SSLv3"
+    },
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    }
   });
 
   const mailOptions = {
     from: `<${process.env.EMAIL_USER}>`,
-    replyTo: `noreply@${process.env.EMAIL_USER.split("@").slice(-1)}`,
+    replyTo: "noreply@anki",
     to,
     subject,
   };
